ba03c0c6feee8325ee8ed0d55dd70ebe3feeb2b9
Simon Gander <simon.gander@ioz.ch>
2024-12-10 15:12:07 +0100

    restructure eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,15 @@
-import { withLibreDash } from "./framework/eslint";
+import withNuxt from "./.playground/.nuxt/eslint.config.mjs";
+import prettier from "eslint-plugin-prettier/recommended";
+import tailwind from "eslint-plugin-tailwindcss";
 
-export default withLibreDash();
+export default withNuxt(
+  {
+    settings: {
+      tailwindcss: {
+        config: useNuxtApp().tailwindcss.configPath,
+      },
+    },
+  },
+  ...tailwind.configs["flat/recommended"],
+  prettier,
+);
